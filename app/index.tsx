e00740f2322f18c57899f76b30b40e0ec9576b60
RecepBorekci <recepborekci29@gmail.com>
2025-08-09 21:24:52 +0300

    Refactor project structure by removing unused components, scripts, and assets. Update layout and routing to include a new HomeScreen. Clean up package.json by removing the reset-project script.

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello 👋</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  text: { fontSize: 18 },
+});
